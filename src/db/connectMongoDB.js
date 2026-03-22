6e80f850907655c1efaac199317962fa547a8399
SANYA3315 <jrchuenko88@gmail.com>
2026-03-22 23:38:18 +0200

    fix2

--- src/db/connectMongoDB.js
+++ src/db/connectMongoDB.js
@@ -3,11 +3,14 @@ import mongoose from 'mongoose';
 export const connectMongoDB = async () => {
   try {
     const mongoUrl = process.env.MONGO_URL;
-    console.log(mongoUrl);
+    if (!mongoUrl) {
+      throw new Error('MONGO_URL is not defined');
+    }
+
     await mongoose.connect(mongoUrl);
     console.log('✅ MongoDB connection established successfully');
   } catch (error) {
     console.error('❌ Failed to connect to MongoDB:', error.message);
-    process.exit(1);
+    process.exit(1); 
   }
 };
